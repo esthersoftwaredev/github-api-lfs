818e53a15127d9470673e2de51ebcd6c42e07883
Esther White <monacodelisa@gmail.com>
2024-08-14 14:48:53 +0300

    angular material and base layout

--- lfs-angular/src/app/app.component.ts
+++ lfs-angular/src/app/app.component.ts
@@ -1,13 +1,26 @@
-import { Component } from '@angular/core';
-import { RouterOutlet } from '@angular/router';
+import { Component, signal } from "@angular/core";
+import { RouterOutlet } from "@angular/router";
+
+import { MatToolbarModule } from "@angular/material/toolbar";
+import { MatExpansionModule } from "@angular/material/expansion";
+import { MatIconModule } from "@angular/material/icon";
+import { MatButtonModule } from "@angular/material/button";
 
 @Component({
-  selector: 'app-root',
-  standalone: true,
-  imports: [RouterOutlet],
-  templateUrl: './app.component.html',
-  styleUrl: './app.component.scss'
+	selector: "app-root",
+	standalone: true,
+	imports: [
+		RouterOutlet,
+		MatToolbarModule,
+		MatExpansionModule,
+		MatButtonModule,
+		MatIconModule,
+	],
+	templateUrl: "./app.component.html",
+	styleUrl: "./app.component.scss",
 })
 export class AppComponent {
-  title = 'lfs-angular';
+	title = "lfs-angular";
+
+  readonly panelOpenState = signal(false);
 }
