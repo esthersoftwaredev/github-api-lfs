9d99dba32b123ff69fd3b10e2129ed3ecc6a6809
Esther White <64324417+monacodelisa@users.noreply.github.com>
2024-08-27 11:12:19 +0300

    adding the checks and returning Boolean

--- connect-four.js
+++ connect-four.js
@@ -1,4 +1,4 @@
-// 0 empty, 1 yellow 2 red
+// 0 empty, player 1 yellow, player 2 red
 
 const board = [
   [2, 1, 1, 1, 2, 1, 2],
@@ -9,21 +9,57 @@ const board = [
 ]
 
 function isGameOver(board) {
-  let yellowScore = 0;
-  let redScore = 0;
-  for (let i = 0; i < board.length; i++) {
-    for (let j = 0; j < board[i].length; j++) {
-      if (board[i][j] == 1) {
-        yellowScore += 1;
-        console.log(yellowScore);
-      } else if (board[i][j] == 2) {
-        redScore += 1;
-        console.log(redScore);
-      } else {
-        console.log("empty slot");
+  const rows = board.length;
+  const cols = board[0].length;
+
+  // Horizontal check
+  for (let i = 0; i < rows; i++) {
+    for (let j = 0; j < cols - 4; j++) {
+
+      if (board[i][j] !== 0 && board[i][j] === board[i][j + 1] && board[i][j] === board[i][j + 2] && board[i][j] === board[i][j + 3]) {
+        // return board[i][j]; 
+        return true;
       }
     }
   }
+
+  // Vertical check
+  for (let j = 0; j < cols; j++) {
+    for (let i; i < rows - 4; i++) {
+      if (board[i][j] !== 0 && board[i][j] === board[i + 1][j] && board[i][j] === board[i + 2][j] && board[i][j] === board[i + 3][j]) {
+        // return board[i][j]; 
+        return true;
+      }
+    }
+  }
+
+  // Diagonal check (down-right)
+  for (let i = 0; i <= rows - 4; i++) {
+    for (let j = 0; j <= cols - 4; j++) {
+      if (board[i][j] !== 0 && board[i][j] === board[i + 1][j + 1] && board[i][j] === board[i + 2][j + 2] && board[i][j] === board[i + 3][j + 3]) {
+        // return board[i][j]; 
+        return true;
+      }
+    }
+  }
+
+  // Diagonal check (down-left)
+  for (let i = 0; i <= rows - 4; i++) {
+    for (let j = 3; j < cols; j++) {
+      if (board[i][j] !== 0 && board[i][j] === board[i + 1][j - 1] && board[i][j] === board[i + 2][j - 2] && board[i][j] === board[i + 3][j - 3]) {
+        // return board[i][j]; 
+        return true;
+      }
+    }
+  }
+
+  return false;
+
 }
 
-isGameOver(board);
+const result = isGameOver(board);
+if (result) {
+  console.log("Game over, we have a winner!");
+} else {
+  console.log("No winner yet.");
+}
